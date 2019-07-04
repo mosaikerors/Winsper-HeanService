03f1c473225b0f14aedb4fbd9cc66e3445476dc4
sjyjytu <1332372004@qq.com>
2019-07-04 12:54:40 +0800

    fix: make it a micro service

--- src/main/java/com/mosaiker/heanservice/HeanServiceApplication.java
+++ src/main/java/com/mosaiker/heanservice/HeanServiceApplication.java
@@ -3,8 +3,10 @@ package com.mosaiker.heanservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaClient
 public class HeanServiceApplication {
 
     public static void main(String[] args) {
