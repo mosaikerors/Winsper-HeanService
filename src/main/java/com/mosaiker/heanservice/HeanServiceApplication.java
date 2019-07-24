5dd424c21ad81f07fa78896d7c1a1f505c06ad93
DeeEll-X <48397449+DeeEll-X@users.noreply.github.com>
2019-07-24 09:28:16 +0800

    fix: enable feign clients

--- src/main/java/com/mosaiker/heanservice/HeanServiceApplication.java
+++ src/main/java/com/mosaiker/heanservice/HeanServiceApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableHystrix
 @RefreshScope
+@EnableFeignClients
 public class HeanServiceApplication {
 
     public static void main(String[] args) {
