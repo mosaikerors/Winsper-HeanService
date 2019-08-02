1eaaeac43885a120411baf65b952da657d0e9248
kty <1332372004@qq.com>
2019-08-02 12:59:56 +0800

    fix: fix bug

--- src/main/java/com/mosaiker/heanservice/service/UserInfoService.java
+++ src/main/java/com/mosaiker/heanservice/service/UserInfoService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "user-service")
+@FeignClient(value = "winsper-user")
 @Service
 public interface UserInfoService {
 
